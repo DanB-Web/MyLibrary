3c47c6a10f1f80879667ba0335dac6688e87b2e6
Dan <dan.bayford@gmail.com>
2021-06-02 11:21:32 +0100

    fix: make new 'Book' component

--- frontend/src/components/Book.js
+++ frontend/src/components/Book.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 const Book = ({book}) => {
+
+  console.log(book);
+
   return (
     <div className="book-container neumorph">
       {book.title}
       {book.authorName}
+      
     </div>
   )
 }
